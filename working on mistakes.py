4913e7578b121a6a5884249157043d34e79e5e9b
SirojiddinMohinur <113341801+SirojiddinMohinur@users.noreply.github.com>
2022-09-20 08:54:11 +0500

    lesson work on mistk
    how to correct mistakes

---
+++ working on mistakes.py
@@ -0,0 +1,25 @@
+#
+# son = float(input("Juft son kiriting: "))
+# if son%2==0:
+#     print("Rahmat!")
+# else:
+#     print('Bu son juft emas.')
+
+# yosh = int(input("Yoshingiz nechida?"))
+#
+# if yosh<=4 or yosh>=60:
+#     narh = 0
+# elif yosh < 18:
+#     narh = 10000
+# else:
+#     narh = 20000
+# print(f"Chipta {narh} so'm")
+
+# users = ['alisher1983','aziz','yasina', 'umar']
+#
+# login = input('Yangi login tanlang:' )
+#
+# if login in users:
+#     print('Login band, yangi login tanalng!')
+# else:
+#     print("Xush kelibsiz!")
